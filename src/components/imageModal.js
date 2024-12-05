ef2e4f1137a44e49696faa622afc508f7c5357b7
Hyeon-Jun Jang <guswns531@gmail.com>
2024-12-05 16:51:35 -0500

    전체화면

--- src/components/imageModal.js
+++ src/components/imageModal.js
@@ -2,7 +2,6 @@ import { ReactComponent as LeftArrow } from '../assets/arrow-left.svg';
 import { ReactComponent as RightArrow } from '../assets/arrow-right.svg';
 
 const ImageModal = ({ clickedImg, handleRotationRight, handleRotationLeft, setClickedImg }) => {
-
     const handleClick = (e) => {
         if (e.target.classList.contains("dismiss")) {
             setClickedImg(null);
@@ -11,17 +10,14 @@ const ImageModal = ({ clickedImg, handleRotationRight, handleRotationLeft, setCl
 
     return (
       <div className="overlay dismiss" onClick={handleClick}>
-        <img className="" src={clickedImg} alt="original size"/>
+        <img src={clickedImg} alt="original size"/>
         <span className="dismiss" onClick={handleClick}>X</span>
-        <div onClick={handleRotationRight}>
-          <div>
-            <RightArrow className="overlay-arrows_right" width="50" height="50" />
-          </div>
+        
+        <div className="overlay-arrows_right" onClick={handleRotationRight}>
+          <RightArrow />
         </div>
-        <div onClick={handleRotationLeft}>
-          <div>
-            <LeftArrow className="overlay-arrows_left" width="50" height="50" />
-          </div>
+        <div className="overlay-arrows_left" onClick={handleRotationLeft}>
+          <LeftArrow />
         </div>
       </div>
     );
